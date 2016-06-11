40071eebcec5af8bf4b467ef85e0995c194ae5b2
Shon Thomas <shonpt@gmail.com>
2016-06-10 23:24:34 -0800

    Added more RWD design support and resolved some bugs in the layout and sizing of boxing

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
-$('document').ready(function() {    
+$('document').ready(function() {
     // Image slides
     var slides = ['#slideOne', '#slideTwo', '#slideThree',
                   '#slideFour', '#slideFive', '#slideSix'];
     
-    // Slide text
+    // Text slides
     var subText = ['#slideOne-subText', '#slideTwo-subText', '#slideThree-subText',
-                  '#slideFour-subText', '#slideFive-subText', '#slideSix-subText'];        
+                  '#slideFour-subText', '#slideFive-subText', '#slideSix-subText'];
     
-    for (var i=0; i < slides.length; i++) {        
-        // Show the slide text on mouse enter
+    for (var i=0; i < slides.length; i++) {
+        // Hide the slide img on mouse enter and show text
         $(slides[i]).bind('mouseenter', function() {
-            $(this).slideUp('slow', function() {
+            //
+            $(this).css('border-bottom', '15px solid #503f50');
+            
+            //            
+            var slide = $(this).attr('id');            
+            slide = '#' + slide + '-subText';
+            
+            //
+            for (var x=0; x < subText.length; x++) {
                 //
-                var slide = $(this).attr('id');
-                slide += '-subText';
-                slide = '#' + slide;
-                
-                //
-                for (var x=0; x < subText.length; x++) {
-                    if (slide == subText[x]) {
-                        $(slide).css('display', 'block');
-                    }
+                if (slide == subText[x]) {
+                    //
+                    $(slide).fadeIn('slowly');
                 }
-            });
+            }
+            
+            //
+            $(this).slideUp('slow');
+            
         });
         
-        // Show the slide image on mouse leave
+        // Show the slide image on mouse leave and hide text
         $(subText[i]).bind('mouseleave', function() {
-            $(this).css('display', 'none');
+            //
+            $(this).fadeOut('slowly');
+            
             //
             var slide = $(this).attr('id');
             slide = '#' + slide;
             slide = slide.split('-');
             
             //
-            $(slide[0]).fadeIn('slow');
+            $(slide[0]).slideDown('slow', function() {
+                $(slide[0]).css('border-bottom', 'none');
+            });            
         });
-    }    
+    }
 });
